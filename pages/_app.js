95c1c517dc6fce953d33c261f60ffe6de783091d
AadhavanPL <aadhavanlenin@gmail.com>
2022-10-24 18:35:45 +0530

    initial files

---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+import 'styles/variables.css'
+import 'styles/globals.css'
+import 'styles/text-styles.css'
+import 'styles/layout.css'
+import 'styles/form.css'
+
+function MyApp({ Component, pageProps }) {
+	return <Component {...pageProps} />
+}
+
+export default MyApp
