e21182c5e3b1cdee4b5c8f4f6cad42848383327b
AadhavanPL <aadhavanlenin@gmail.com>
2022-10-30 14:28:10 +0530

    total dn buy button working

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6b0ee7ceaa64dd38"; }
+/******/ 		__webpack_require__.h = function() { return "5d85564fd2e22697"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/buy.js
+++ pages/buy.js
@@ -1,28 +1,19 @@
 import Navbar from 'components/common/Navbar'
-import React from 'react'
-import { useState } from 'react'
+import React, { useEffect } from 'react'
+import { useState, useRef } from 'react'
 import { motion } from 'framer-motion'
 
 import styles from '../components/buy.module.css'
 
 export default function BuyPage() {
 	const [openFaq, setOpenFaq] = useState('')
+	const quantity = useRef(null)
+	const total = useRef(null)
 	const data = [
 		{ name: 'Bitcoin', tag: 'BTC', price: '18,546', inc: '5.76', dec: '', marketcap: '18.0T' },
 		{ name: 'Apple Inc.', tag: 'AAPL', price: '223.17', inc: '', dec: '2.13', marketcap: '12.7T' },
-		// { name: 'Bitcoin', tag: 'BTC', price: '18,546', inc: '5.76', dec: '', marketcap: '18.0T' },
-		// { name: 'Apple Inc.', tag: 'AAPL', price: '223.17', inc: '', dec: '2.13', marketcap: '12.7T' },
-		// { name: 'Bitcoin', tag: 'BTC', price: '18,546', inc: '5.76', dec: '', marketcap: '18.0T' },
-		// { name: 'Apple Inc.', tag: 'AAPL', price: '223.17', inc: '', dec: '2.13', marketcap: '12.7T' },
-		// { name: 'Bitcoin', tag: 'BTC', price: '18,546', inc: '5.76', dec: '', marketcap: '18.0T' },
-		// { name: 'Apple Inc.', tag: 'AAPL', price: '223.17', inc: '', dec: '2.13', marketcap: '12.7T' },
-		// { name: 'Bitcoin', tag: 'BTC', price: '18,546', inc: '5.76', dec: '', marketcap: '18.0T' },
-		// { name: 'Apple Inc.', tag: 'AAPL', price: '223.17', inc: '', dec: '2.13', marketcap: '12.7T' },
-		// { name: 'Bitcoin', tag: 'BTC', price: '18,546', inc: '5.76', dec: '', marketcap: '18.0T' },
-		// { name: 'Apple Inc.', tag: 'AAPL', price: '223.17', inc: '', dec: '2.13', marketcap: '12.7T' },
-		// { name: 'Bitcoin', tag: 'BTC', price: '18,546', inc: '5.76', dec: '', marketcap: '18.0T' },
-		// { name: 'Apple Inc.', tag: 'AAPL', price: '223.17', inc: '', dec: '2.13', marketcap: '12.7T' },
 	]
+
 	return (
 		<div className={styles['container']}>
 			<Navbar>
@@ -77,7 +68,42 @@ export default function BuyPage() {
 									opacity: openFaq == item.tag ? 1 : 0,
 									height: openFaq == item.tag ? 'auto' : 0,
 								}}
-							></motion.div>
+							>
+								<div className={styles['stock-bottom-left']}>
+									<div className={styles['input-container']}>
+										<div className={styles['input-wrapper']}>
+											<label>Quantity</label>
+											<input
+												id={item.tag}
+												ref={quantity}
+												placeholder='Enter number of shares'
+												type='number'
+												min={0}
+											/>
+										</div>
+										<img
+											src='/refresh.svg'
+											onClick={() => {
+												total.current.innerHTML = quantity.current.value * item.price
+											}}
+										/>
+									</div>
+									<div className={styles['total-wrapper']}>
+										Total
+										<span ref={total}>$0</span>
+									</div>
+								</div>
+
+								<div
+									className={styles['buy-btn']}
+									onClick={() => {
+										total.current.innerHTML = quantity.current.value * item.price
+									}}
+								>
+									<span>Buy</span>
+									<img src='/arrow-right.svg' />
+								</div>
+							</motion.div>
 						</div>
 					))}
 				</div>
